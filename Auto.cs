da0d8d39746a3f42841cc89c600b85222008a753
DaviesNyantakyi <davies.nyantakyi@outlook.com>
2024-08-03 04:02:55 +0200

    -creating classes -overriding classes

---
+++ Auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace auto
+{
+    public class Auto
+    {
+        public string merk = "Tesla model S";
+        public string kleur= "Wit";
+        public int aantalDeuren = 4;
+        public int maxSnelheid = 200;
+        public int pk  = 100;
+
+        public Auto(string merk, string kleur, int aantalDeuren, int maxSnelheid)
+        {
+            this.merk = merk;
+            this.kleur = kleur;
+            this.aantalDeuren = aantalDeuren;
+            this.maxSnelheid = maxSnelheid;
+        }
+
+        // Constuctor overloading met alleen de merk 
+        public Auto(string merk)
+        {
+            this.merk = merk;
+
+        }
+
+        public Auto(int aantalDeuren, int maxSnelheid )
+        {
+            this.aantalDeuren = aantalDeuren;
+            this.maxSnelheid = maxSnelheid;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using auto; // When you want to use the class auto. You have to reference to the namespace in the Auto.cs file
+using System;
+
+
+namespace lesOOP
+{
+
+  class Program
+    {
+        static void Main() {
+
+
+            Auto auto = new Auto(merk:"Tesla model 3", kleur: "Wit",aantalDeuren: 4,maxSnelheid: 200 );
+
+            // Auto object met alleen de deuren en maxSnelheid
+            Auto auto2 = new Auto(aantalDeuren: 4, maxSnelheid: 300);
+            // Auto object met alleen de 
+            Auto auto3 = new Auto(merk: "Range rover");
+            Console.WriteLine(auto2.merk);
+        }
+    }
+  
+}
